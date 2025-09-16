61cc9a95b1bafc6c5d6f936127ddd4d6739bd232
jacquikeating <j.keating@live.ca>
2025-09-15 23:48:08 -0400

    Imported pages to App and defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
