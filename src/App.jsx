79483b1331e1a22aadc3b58484bba1d337264f45
jacquikeating <j.keating@live.ca>
2025-09-17 22:14:45 -0400

    Added global header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
 
   return (
     <BrowserRouter>
+    <header>
+      <Link className="site-logo" to="/">#VanLife</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
