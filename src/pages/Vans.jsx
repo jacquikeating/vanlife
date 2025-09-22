aca8282497e4e8a52be8635e328cf1363a8500d3
jacquikeating <j.keating@live.ca>
2025-09-22 10:01:20 -0400

    Added vans state variable

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Vans() {
+    const [vans, setVans] = useState([])
+
 
 	useEffect(() => {
 	  fetch("/api/vans")
 	    .then(res => res.json())
-	    .then(data => console.log(data))
+	    .then(data => setVans(data.vans))
 	}, [])
 	
 	return (<h1>Vans page goes here 🚐</h1>)
