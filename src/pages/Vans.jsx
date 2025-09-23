c5ade774d4efb0eebf63d742a313149a1a1dc2bf
jacquikeating <j.keating@live.ca>
2025-09-23 00:08:11 -0400

    Created a .map() to render content from vans data

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -3,12 +3,24 @@ import { useEffect, useState } from "react"
 export default function Vans() {
     const [vans, setVans] = useState([])
 
-
 	useEffect(() => {
 	  fetch("/api/vans")
 	    .then(res => res.json())
 	    .then(data => setVans(data.vans))
 	}, [])
 	
-	return (<h1>Vans page goes here 🚐</h1>)
+	return (
+        <>
+            <h1>Explore our van options</h1>
+            
+            {vans.map(van => (
+                <div key={van.id}>
+                    <img src={van.imageUrl} alt={`${van.name} van parked outdoors.`}/>
+                    <h2>{van.name}</h2>
+                    <p>${van.price}/day</p>
+                    <p>{van.type}</p>
+                </div>
+            ))}
+	    </>
+    )
 }
