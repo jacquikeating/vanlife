2878d4bbb139e1aa8576f0d696fcf984b389ca96
jacquikeating <j.keating@live.ca>
2025-09-28 02:07:02 -0400

    Added useParams to VanDetail page

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,3 +1,7 @@
-export default function VanDetail() {
-    return <h1>Van detail page goes here</h1>
+import { useParams } from "react-router-dom"
+
+    export default function VanDetail() {
+        const { vanID } = useParams()
+        
+        return <h1>Van detail page goes here</h1>
 }
