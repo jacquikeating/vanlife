1d1994e9bf23333b3d2146cc9bd08185b1a4f04d
jacquikeating <j.keating@live.ca>
2025-09-29 23:41:27 -0400

    VanDetail pages automatically fetch data for that van

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,7 +1,16 @@
+import  { useEffect, useState } from "react" 
 import { useParams } from "react-router-dom"
+   
+export default function VanDetail() {
+    const [van, setVan] = useState(null)
+    const { vanID } = useParams()
 
-    export default function VanDetail() {
-        const { vanID } = useParams()
-        
-        return <h1>Van detail page goes here</h1>
+    useEffect(() => {
+        fetch(`/api/vans/${vanID}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [vanID])
+
+
+    return <h1>Van detail page goes here</h1>
 }
