fa91a4c63a3288e72aa3a8f86859dae1f85f2f65
jacquikeating <j.keating@live.ca>
2025-10-09 21:59:12 -0400

    Improved route nesting

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/host" element={<HostLayout />}>
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          
+          <Route path="vans">
+            <Route index element={<Vans />} />
+            <Route path=":id" element={<VanDetail />} />
+          </Route>
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
