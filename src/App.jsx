3a31692fb48a6207689bb3498e1b5661f72390ec
jacquikeating <j.keating@live.ca>
2025-10-12 13:52:55 -0400

    Set up routes for HostVans and HostVanDetail pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import HostLayout from "./components/HostLayout"  
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import "./index.css"
 import "./server"
 
 function App() {
@@ -25,6 +28,8 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
