c48500c5e633c5eec9502d351b3c6e0603cbc40e
jacquikeating <j.keating@live.ca>
2025-10-12 13:26:48 -0400

    Styled HostLayout nav

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom"
 export default function HostLayout() {
     return (
         <>
-            <nav>
+            <nav className="host-nav">
                 <Link to="/host">Dashboard</Link>
                 <Link to="/host/income">Income</Link>
                 <Link to="/host/reviews">Reviews</Link>
