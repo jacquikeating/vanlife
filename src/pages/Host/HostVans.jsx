3735d7c6dad7f9e3718b5b07e07f924cc08989fa
jacquikeating <j.keating@live.ca>
2025-10-12 14:09:37 -0400

    HostVans page fetches data from mock API

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,4 +1,14 @@
+import React from "react"
+
 export default function HostVans() {
+const [vans, setVans] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
     return (
         <h1>HostVans page goes here</h1>
     )
