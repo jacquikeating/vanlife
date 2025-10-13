d0b43c091d994c7a383edca92c9b75ad50c83e16
jacquikeating <j.keating@live.ca>
2025-10-13 11:34:39 -0400

    HostVanDetail fetches API data

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,4 +1,16 @@
+import React from "react"
+import { useParams } from "react-router-dom"
+
 export default function HostVanDetail() {
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => console.log(data.vans[0]))
+    }, [])
+
     return (
         <h1>HostVanDetail page goes here</h1>
     )
