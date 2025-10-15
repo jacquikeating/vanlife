96f8d33f8596d4d0cec73a2d90b80292cfd5b923
jacquikeating <j.keating@live.ca>
2025-10-15 13:04:31 -0400

    Added JSX content to HostVanInfo

--- src/pages/Host/HostVanInfo.jsx
+++ src/pages/Host/HostVanInfo.jsx
@@ -8,6 +8,11 @@ export default function HostVanInfo() {
     console.log(currentVan)
 
     return (
-        <h1>{currentVan.name}</h1>
+       <section className="host-van-detail-info">
+            <h4>Name: <span>{currentVan.name}</span></h4>
+            <h4>Category: <span>{currentVan.type}</span></h4>
+            <h4>Description: <span>{currentVan.description}</span></h4>
+            <h4>Visibility: <span>Public</span></h4>
+        </section>
     )
 }
