8d67f8e6b669417ed35e6f615882c147d6a254fd
jacquikeating <j.keating@live.ca>
2025-10-16 13:23:17 -0400

    VanDetail useParams() bug fix

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,23 +1,22 @@
-import  { useEffect, useState } from "react" 
+import { useEffect, useState } from "react" 
 import { useParams } from "react-router-dom"
    
 export default function VanDetail() {
     const [van, setVan] = useState(null)
-    const { vanID } = useParams()
+    const params  = useParams()
 
     useEffect(() => {
-        fetch(`/api/vans/${vanID}`)
+        fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
             .then(data => setVan(data.vans))
-    }, [vanID])
-
+    }, [params.id])
 
     return (
         <div className="van-detail-container">
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} alt={`${van.name} van parked outdoors.`} />
-                    <span className={`van-type ${van.type} selected`}>{van.type}</span>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
                     <h2>{van.name}</h2>
                     <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
