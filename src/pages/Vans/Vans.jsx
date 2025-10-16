932f3e3c268cc17dfd3699ec83b7d0d4c622baf7
jacquikeating <j.keating@live.ca>
2025-10-16 13:07:18 -0400

    Vans type filter buttons now call handleFilterChange()

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -42,25 +42,32 @@ export default function Vans() {
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams({type: "simple"})} 
+                <button onClick={() => handleFilterChange("type", "simple")} 
                     className={
                         `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
                     }>
                     Simple
                 </button>
-                <button onClick={() => setSearchParams({type: "luxury"})} 
+                <button onClick={() => handleFilterChange("type", "luxury")}
                     className={
                         `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
                     }>
                     Luxury
                 </button>
-                <button onClick={() => setSearchParams({type: "rugged"})} 
+                <button onClick={() => handleFilterChange("type", "rugged")}
                     className={
                         `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
                     }>
                     Rugged
                 </button>
-                {typeFilter && <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filters</button>}
+                {typeFilter && 
+                    <button 
+                        onClick={() => handleFilterChange("type", null)} 
+                        className="van-type clear-filters"
+                        >
+                        Clear filters
+                    </button>
+                }
             </div>
             <div className="van-list">
                 {vanElements}
