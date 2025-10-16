c01b9eafe826f421b942a666dd1e38dfe25d2185
jacquikeating <j.keating@live.ca>
2025-10-16 11:46:29 -0400

    Applied conditional styling to Vans type filter buttons

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -31,9 +31,24 @@ export default function Vans() {
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button onClick={() => setSearchParams({type: "simple"})} className="van-type simple">Simple</button>
-                <button onClick={() => setSearchParams({type: "luxury"})} className="van-type luxury">Luxury</button>
-                <button onClick={() => setSearchParams({type: "rugged"})} className="van-type rugged">Rugged</button>
+                <button onClick={() => setSearchParams({type: "simple"})} 
+                    className={
+                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                    }>
+                    Simple
+                </button>
+                <button onClick={() => setSearchParams({type: "luxury"})} 
+                    className={
+                        `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                    }>
+                    Luxury
+                </button>
+                <button onClick={() => setSearchParams({type: "rugged"})} 
+                    className={
+                        `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                    }>
+                    Rugged
+                </button>
                 {typeFilter && <button onClick={() => setSearchParams({})} className="van-type clear-filters">Clear filters</button>}
             </div>
             <div className="van-list">
