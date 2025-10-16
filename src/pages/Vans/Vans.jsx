f816099603e74aa3b056bccfbc87e3ac5a013d37
jacquikeating <j.keating@live.ca>
2025-10-16 13:23:05 -0400

    Vans page vanElements each include a Link to VanDetailPage

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams, Link } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = useState([])
@@ -18,12 +18,14 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} alt={`${van.name} van parked outdoors.`} />
-            <div className="van-info">
-                <h2>{van.name}</h2>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <span className={`van-type ${van.type} selected`}>{van.type}</span>
+            <Link to={van.id}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
