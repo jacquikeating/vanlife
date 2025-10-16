f108342f229249d685ef44218bc78f3898378a4b
jacquikeating <j.keating@live.ca>
2025-10-16 13:30:34 -0400

    Added "back to all vans" link on VanDetail page

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react" 
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
    
 export default function VanDetail() {
     const [van, setVan] = useState(null)
@@ -13,6 +13,12 @@ export default function VanDetail() {
 
     return (
         <div className="van-detail-container">
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
+
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} alt={`${van.name} van parked outdoors.`} />
