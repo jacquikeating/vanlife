c7b96e6fcdcd7ea29e8861522d0afb8274df16bc
jacquikeating <j.keating@live.ca>
2025-10-18 20:41:50 -0400

    Replaced placeholder content on NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function NotFound() {
     return (
-        <p>404</p>
+        <>
+            <h1>Sorry, the page you were looking for was not found.</h1>
+            <Link to="/">Return to Home</Link>
+        </>
     )
 };
