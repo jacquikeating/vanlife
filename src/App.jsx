983d5710d387449ee1e6a3edf182fbaced1cfe43
jacquikeating <j.keating@live.ca>
2025-10-18 20:40:43 -0400

    Set up catch-all 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import HostVanDetail from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import HostVanPricing from "./pages/Host/HostVanPricing"
+import NotFound from "./pages/NotFound"
 import "./index.css"
 import "./server"
 
@@ -38,6 +39,8 @@ function App() {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
