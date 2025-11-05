01f52d5684dd8157a92c7068e11ae5b96d83d3be
jacquikeating <j.keating@live.ca>
2025-11-04 18:19:53 -1000

    Added Firebase config to api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1ED2x9QDOUMGLAQ4pwFFslOvZoOO_BNE",
+  authDomain: "vanlife-86f65.firebaseapp.com",
+  projectId: "vanlife-86f65",
+  storageBucket: "vanlife-86f65.firebasestorage.app",
+  messagingSenderId: "746842630873",
+  appId: "1:746842630873:web:f6c6d26efaacb43b656cc8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
 export async function getVans() {
     const res = await fetch("/api/vans")
     if (!res.ok) {
