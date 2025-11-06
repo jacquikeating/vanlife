38758462aa478b6fb864bfea4f8d02ceb400bdb6
jacquikeating <j.keating@live.ca>
2025-11-05 22:00:27 -1000

    Switched to lite Firestore package

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD1ED2x9QDOUMGLAQ4pwFFslOvZoOO_BNE",
