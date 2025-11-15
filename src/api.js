03d45fcf8955f9757ec0938f6e2584e0d409fb96
jacquikeating <j.keating@live.ca>
2025-11-14 22:39:36 -0700

    Added getAuth() from Firebase Auth

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc, query, where, doc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD1ED2x9QDOUMGLAQ4pwFFslOvZoOO_BNE",
@@ -12,6 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+const auth = getAuth(app);
 const vansCollectionRef = collection(db, "vans")
 
 export async function getVans() {
